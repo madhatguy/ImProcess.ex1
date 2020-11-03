ab0226f98b2d17eec337210f36f7d31b0547c609
thehatguy <guy.lev@mail.huji.ac.il>
2020-11-04 01:10:42 +0200

    deleted all prints

--- sol1.py
+++ sol1.py
@@ -10,10 +10,13 @@ from skimage.color import rgb2gray
 WRONG_PATH_MSG = "Error: bad filename"
 MAX_VAL = 255
 SHADE_LEVELS = 256
+FIRST_IDX = 0
+LAST_IDX = -1
+SQUARE_POWER = 2
 GRAY = 1
-RED_CHAN = 0
-GREEN_CHAN = 1
-BLUE_CHAN = 2
+CHAN_1 = 0
+CHAN_2 = 1
+CHAN_3 = 2
 YIQ = np.array([[0.299, 0.587, 0.114],
                 [0.596, -0.275, -0.321],
                 [0.212, -0.523, 0.311]], dtype="float64")
@@ -31,7 +34,6 @@ def read_image(filename, representation):
     im_arr = imio.imread(filename)
     im_arr = im_arr.astype("float64")
     im_arr /= MAX_VAL
-    im_arr /= 3  # todo - delete?
     if representation == GRAY:  # todo - check whether the pic is already grey?
         im_arr = rgb2gray(im_arr)
     return im_arr
@@ -45,7 +47,7 @@ def im_data_disp(im_arr, representation):
     :return: None
     """
     if representation == GRAY:
-        plt.imshow(im_arr, cmap=plt.cm.gray)  # todo - change to use only Y channel
+        plt.imshow(im_arr[..., [FIRST_IDX]], cmap=plt.cm.gray)
     else:
         plt.imshow(im_arr)
     plt.show()
@@ -67,9 +69,9 @@ def rgb2yiq(imRGB):
     :param imRGB: An array containing the original image data
     :return: An array containing the modified image data
     """
-    y = np.inner(YIQ[RED_CHAN], imRGB)
-    i = np.inner(YIQ[GREEN_CHAN], imRGB)
-    q = np.inner(YIQ[BLUE_CHAN], imRGB)
+    y = np.inner(YIQ[CHAN_1], imRGB)
+    i = np.inner(YIQ[CHAN_2], imRGB)
+    q = np.inner(YIQ[CHAN_3], imRGB)
     return np.dstack((y, i, q))
 
 
@@ -80,9 +82,9 @@ def yiq2rgb(imYIQ):
     :return: An array containing the modified image data
     """
     inv_yiq = np.linalg.inv(YIQ)
-    r = np.inner(imYIQ, inv_yiq[RED_CHAN])
-    g = np.inner(imYIQ, inv_yiq[GREEN_CHAN])
-    b = np.inner(imYIQ, inv_yiq[BLUE_CHAN])
+    r = np.inner(imYIQ, inv_yiq[CHAN_1])
+    g = np.inner(imYIQ, inv_yiq[CHAN_2])
+    b = np.inner(imYIQ, inv_yiq[CHAN_3])
     return np.dstack((r, g, b))
 
 
@@ -93,25 +95,14 @@ def histogram_equalize(im_orig):
     :return: A list containing the modified image, the original histogram and the histogram after the equalization
     """
     grayscale = rgb2yiq(im_orig)  # todo - check whether the image is YIQ?
-    y = (grayscale[..., RED_CHAN].copy() * MAX_VAL).astype("uint8")
+    y = (grayscale[..., CHAN_1].copy() * MAX_VAL).astype("uint8")
     orig_hist, bins = np.histogram(y, SHADE_LEVELS)
     cum_hist = np.cumsum(orig_hist)
-    min_val_amount = cum_hist[orig_hist.nonzero()[0][0]]
-    lut = (MAX_VAL * (cum_hist - min_val_amount) / (cum_hist[-1] - min_val_amount)).round().astype("uint8")
-    grayscale[..., RED_CHAN] = lut[y] / lut[y].max()  # todo - is this the right division?
+    min_val_amount = cum_hist[orig_hist.nonzero()[FIRST_IDX][FIRST_IDX]]
+    lut = (MAX_VAL * (cum_hist - min_val_amount) / (cum_hist[LAST_IDX] - min_val_amount)).round().astype("uint8")
+    grayscale[..., CHAN_1] = lut[y] / lut[y].max()  # todo - is this the right division?
     res = yiq2rgb(grayscale)
-    return [res, orig_hist, np.histogram(y, SHADE_LEVELS)[0]]
-
-
-def quant_error(orig_hist, z, y, q, i):
-    """
-
-    :param orig_hist:
-    :param z:
-    :param y:
-    :param q:
-    :return:
-    """
+    return [res, orig_hist, np.histogram(y, SHADE_LEVELS)[FIRST_IDX]]
 
 
 def quantize(im_orig, n_quant, n_iter):
@@ -123,11 +114,10 @@ def quantize(im_orig, n_quant, n_iter):
     :return: A list containing the modified image and an array of the error of each iteration
     """
     grayscale = rgb2yiq(im_orig)
-    y = (grayscale[..., RED_CHAN].copy() * MAX_VAL).astype("uint8")
+    y = (grayscale[..., CHAN_1].copy() * MAX_VAL).astype("uint8")
     orig_hist, bins = np.histogram(y, SHADE_LEVELS)
-    z = np.quantile(y, np.linspace(0, 1, n_quant + 1)).astype("uint8")  # todo - smart choose
+    z = np.quantile(y, np.linspace(FIRST_IDX, 1, n_quant + 1)).astype("uint8")  # todo - smart choose
     q = np.empty(n_quant)
-    print(z)
     error = np.zeros(n_iter)
     for it in range(n_iter):
         prev_z = z.copy()
@@ -135,20 +125,14 @@ def quantize(im_orig, n_quant, n_iter):
             denum = orig_hist[z[i]:z[i + 1]].sum()
             num = sum(orig_hist[cur] * cur for cur in range(z[i], z[i + 1]))
             q[i] = round(num / denum)  # todo - no round?
-            error[it] += sum(orig_hist[cur] * (cur - q[i]) ** 2 for cur in range(z[i], z[i + 1]))
-            if np.isnan(q[i]):  # todo - why nan when n_quant == 20?
-                print(z[i])
-                print(orig_hist[z[i]:z[i + 1]])
-                return [None, None]
-        print(z)
+            error[it] += sum(orig_hist[cur] * (cur - q[i]) ** SQUARE_POWER for cur in range(z[i], z[i + 1]))
         for i in range(n_quant - 1):
             z[i] = ((q[i] + q[i + 1]) / 2)
         if (z == prev_z).all:
             break
     lut = [q[i] for i in range(n_quant) for j in range(z[i], z[i+1])]
-    rem = [q[-1]] * (SHADE_LEVELS - len(lut))
+    rem = [q[LAST_IDX]] * (SHADE_LEVELS - len(lut))
     lut = np.array(lut + rem)
-    print(lut.shape)
-    grayscale[..., RED_CHAN] = lut[y] / MAX_VAL
+    grayscale[..., CHAN_1] = lut[y] / MAX_VAL
     im_quant = yiq2rgb(grayscale)
     return [im_quant, np.trim_zeros(error)]
